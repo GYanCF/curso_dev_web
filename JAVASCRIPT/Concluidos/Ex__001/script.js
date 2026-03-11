9256689896b6560cb973c5fed11578c1735284cc
walissonalves-dev <gabriel.yancf@gmail.com>
2026-03-10 21:20:11 -0300

    v11

---
+++ JAVASCRIPT/Concluidos/Ex__001/script.js
@@ -0,0 +1,13 @@
+/* diferença mais em locals
+var: (GLOBAL)
+let: (FUNC)
+const: ( FUNC / IMUTAVEL )
+*/
+
+
+var x = 2;
+var y = 3;
+var soma = x + y;
+
+console.log(`a soma de x e y é: ${soma}`);
+console.log(`a soma de ${x} e ${y} é: ${soma}`);
---
+++ JAVASCRIPT/Ex__003/script.js
@@ -0,0 +1,15 @@
+var container = document.getElementById('area')
+
+function clique() {
+    container.innerHTML = `AI KRAI`;
+};
+
+function entrar() {
+    container.innerHTML = `Tire essa seta daqui!`;
+    container.style.background = "blue";
+};
+
+function sair() {
+    container.innerHTML = `Obrigado!`;
+
+};
